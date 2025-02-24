a5258d1550f360b8c25cec8f053295d2370a7c05
Rahul Nihalani <itsnrahul@gmail.com>
2025-02-24 22:23:47 +0530

    Data Validation Completed

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -9,7 +9,7 @@ import pickle
 
 def read_yaml_file(file_path: str) -> dict:
     try:
-        with open(file_path, "rb") as yaml_file:
+        with open(file_path, 'rb') as yaml_file:
             return yaml.safe_load(yaml_file)
     except Exception as e:
         raise NetworkSecurityException(e, sys)
@@ -20,7 +20,7 @@ def write_yaml_file(file_path: str, content: object, replace: bool = False) -> N
             if os.path.exists(file_path):
                 os.remove(file_path)
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
-        with open(file_path, "w", encoding="utf-8") as file:
+        with open(file_path, 'w') as file:
             yaml.dump(content, file)
     except Exception as e:
         raise NetworkSecurityException(e, sys)
